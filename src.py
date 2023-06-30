3f497d1907d58274312570f67b429b3b0190370e
wk <dnfking83@gmail.com>
2023-06-30 07:52:44 -0700

    Update src.py

--- src.py
+++ src.py
@@ -5,7 +5,7 @@ from pymem.pattern import *
 from helpers import *
 import requests
 import numpy as np
-
+import math
 
 exc_name = 'csgo.exe'
 exc_title = 'Counter-Strike: Global Offensive - Direct3D 9'
@@ -48,6 +48,7 @@ class entity:
         self.armor = pm.read_int(self.addr + Offsets.m_ArmorValue)
         self.team = pm.read_int(self.addr + Offsets.m_iTeamNum)
         self.bone_base = pm.read_int(self.addr + Offsets.m_dwBoneMatrix)
+        self.bDormant = pm.read_bool(self.addr + Offsets.m_bDormant)
     def bone_pos(self, bone_id):
         return Vec3(
             pm.read_float(self.bone_base + 0x30 * bone_id + 0x0C),
@@ -76,12 +77,40 @@ def calculate_mid_vec(pass1, pass2):
     else:
         return pass2 - pass1
 
+def calculate_distance(point1, point2):
+    x1, y1 = point1
+    x2, y2 = point2
+    distance = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+    return distance
+
 def draw_custom_box_2d(BLeft, BRight, Top, Thickness=1, Filled=False, Color=RED):
     draw_line_ex(Vector2(int(BLeft[0]), int(BLeft[1])), Vector2(int(BLeft[0]), int(Top[1])), Thickness, Color) # Left Line
     draw_line_ex(Vector2(int(BRight[0]), int(BRight[1])), Vector2(int(BRight[0]), int(Top[1])), Thickness, Color) # Right Line
 
     draw_line_ex(Vector2(int(BLeft[0]), int(Top[1])), Vector2(int(BRight[0]), int(Top[1])), Thickness, Color) # Top
     draw_line_ex(Vector2(int(BLeft[0]), int(BLeft[1])), Vector2(int(BRight[0]), int(BRight[1])), Thickness, Color) # Bottom
+
+def skeleton_esp(viewMatrix, entity_class):
+    ConnectedPoints = [
+        '8|7|6|5|3', # Head->TopChest->MiddleChest->Lowe
+        '7|38|64|39|62|50', # TopChest->LeftArm->Lefthnad
+        '7|10|35|36|11|34|12|14', #TopChest->RightArm->Rig
+        '3|72|77|78|73|74|75', # LowerChest->Leg Left
+        '3|65|70|71|66|67|68', # LowerChest->Leg Right
+    ]
+    # Skeleton ESP #
+    for x in ConnectedPoints:
+        expanded = x.split(sep='|')
+        for i in range(len(expanded) - 1):  # Iterate up to len(expanded) - 1
+            try:
+                bone1 = worldToScreen(viewMatrix, entity_class.bone_pos(bone_id=int(expanded[i])), 1)
+                bone2 = worldToScreen(viewMatrix, entity_class.bone_pos(bone_id=int(expanded[i + 1])), 1)
+                distance_threshold = 50  # Adjust the threshold value as needed
+                distance = calculate_distance(bone1, bone2)
+                if distance <= distance_threshold:
+                    draw_line(int(bone1[0]), int(bone1[1]), int(bone2[0]), int(bone2[1]), WHITE)
+            except:
+                pass
 
 def Main():
     font = load_font_ex("fonts\\Arial_Bold.ttf", 16, None, 3260)
@@ -90,58 +119,44 @@ def Main():
         if (is_window_active(exc_title)):
             begin_drawing()
             update_window_pos(exec_title=exc_title)
-            draw_fps(0, 250)
-            for i in range(1, 32):
-                try:
-                    entity_addr = pm.read_int(clientModule + Offsets.dwEntityList + i * 0x10)
-                    local_addr = pm.read_int(clientModule + Offsets.dwLocalPlayer)
-                    if entity_addr:
-                        local_class = entity(mem=pm, addr=local_addr, module=clientModule)
-                        entity_class = entity(mem=pm, addr=entity_addr, module=clientModule)
-                        if entity_class.health > 0:
-                            viewMatrix = VecMem.read_4x4(addr=clientModule + Offsets.dwViewMatrix, pm=pm)
+            #draw_fps(0, 250)
 
-                            ConnectedPoints = [
-                                '8|7|6|5|3', # Head->TopChest->MiddleChest->LowerChest
+            local_addr = pm.read_int(clientModule + Offsets.dwLocalPlayer)
+            if local_addr != None and local_addr != 0:
+                local_class = entity(mem=pm, addr=local_addr, module=clientModule)
+                for i in range(1, 32):
+                    try:
+                        entity_addr = pm.read_int(clientModule + Offsets.dwEntityList + i * 0x10)
+                        if entity_addr:
+                            entity_class = entity(mem=pm, addr=entity_addr, module=clientModule)
+                            if not entity_class.bDormant and entity_class.health > 0:
+                                viewMatrix = VecMem.read_4x4(addr=clientModule + Offsets.dwViewMatrix, pm=pm)
 
-                                '7|38|64|39|62|50', # TopChest->LeftArm->Lefthnad
-                                '7|10|35|36|11|34|12|14', #TopChest->RightArm->RightHand
-
-                                '3|72|77|78|73|74|75', # LowerChest->Leg Left
-                                '3|65|70|71|66|67|68', # LowerChest->Leg Right
-                            ]
-
-                            # Skeleton ESP #
-                            for x in ConnectedPoints:
-                                expanded = x.split(sep='|')
-                                for i in range(len(expanded) - 1):  # Iterate up to len(expanded) - 1
-                                    bone1 = worldToScreen(viewMatrix, entity_class.bone_pos(bone_id=int(expanded[i])), 1)
-                                    bone2 = worldToScreen(viewMatrix, entity_class.bone_pos(bone_id=int(expanded[i + 1])), 1)
-                                    draw_line(int(bone1[0]), int(bone1[1]), int(bone2[0]), int(bone2[1]), WHITE)
-
-                            # Box ESP #
-                            try:
-                                HeadPos = worldToScreen(viewMatrix, entity_class.bone_pos(bone_id=8), 1)    
-                                LeftFoot = worldToScreen(viewMatrix, entity_class.bone_pos(bone_id=75), 1)
-                                RightFoot = worldToScreen(viewMatrix, entity_class.bone_pos(bone_id=67), 1)       
-                                if HeadPos != None and LeftFoot != None and RightFoot != None:
-                                    draw_custom_box_2d(LeftFoot, RightFoot, HeadPos, 1, False, BLUE if entity_class.team == local_class.team else RED)
-                                    # Health Bar #
-                                    if entity_class.health > 75:
-                                        health_color = GREEN
-                                    elif entity_class.health > 50 and entity_class.health < 75:
-                                        health_color = YELLOW
-                                    elif entity_class.health > 25 and entity_class.health < 50:
-                                        health_color = RED
-                                    elif entity_class.health > 1 and entity_class.health < 25:
-                                        health_color = BLACK
-                                    draw_line(int(LeftFoot[0])-5, int(LeftFoot[1]), int(LeftFoot[0])-5, int(HeadPos[1]), health_color)
-                            except Exception as e:
-                                if debug==True:
-                                    print(e)
-                except Exception as e:
-                    if debug==True:
-                        print(e)
+                                # Skeelton ESP #
+                                skeleton_esp(viewMatrix, entity_class)
+                                # Box ESP #
+                                try:
+                                    HeadPos = worldToScreen(viewMatrix, entity_class.bone_pos(bone_id=8), 1)    
+                                    LeftFoot = worldToScreen(viewMatrix, entity_class.bone_pos(bone_id=75), 1)
+                                    RightFoot = worldToScreen(viewMatrix, entity_class.bone_pos(bone_id=67), 1)       
+                                    if HeadPos != None and LeftFoot != None and RightFoot != None:
+                                        draw_custom_box_2d(LeftFoot, RightFoot, HeadPos, 1, False, BLUE if entity_class.team == local_class.team else RED)
+                                        # Health Bar #
+                                        if entity_class.health > 75:
+                                            health_color = GREEN
+                                        elif entity_class.health > 50 and entity_class.health < 75:
+                                            health_color = YELLOW
+                                        elif entity_class.health > 25 and entity_class.health < 50:
+                                            health_color = RED
+                                        elif entity_class.health > 1 and entity_class.health < 25:
+                                            health_color = BLACK
+                                        draw_line(int(LeftFoot[0])-5, int(LeftFoot[1]), int(LeftFoot[0])-5, int(HeadPos[1]), health_color)
+                                except Exception as e:
+                                    if debug==True:
+                                        print(e)
+                    except Exception as e:
+                        if debug==True:
+                            print(e)
             end_drawing()
         else:
             end_drawing()
